8db9720538db32b4072240ad4251fefdf68abdc3
HaseFlrn <florianhase12@gmail.com>
2023-03-24 18:45:45 +0100

    profile landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,39 @@
-<main><h1>Hello</h1></main>
+<script lang="ts">
+	import JsonViewer from "$lib/components/JsonViewer.svelte";
+    import UserController from "$lib/controllers/UserController";
+	import { onMount } from "svelte";
+	import { writable } from "svelte/store";
+
+    const {store, newName} = UserController;
+    let existingAccount = writable(true);
+
+    function onSubmit() {
+        UserController.register()
+    }
+
+    onMount(async()=> {
+        const t = await UserController.init();
+        existingAccount.set(t == false ? false : true);
+    })
+
+    $: ({ myName,myAddress,myBalance,myContacts, message} = $store);
+</script>
+
+<JsonViewer data={$store}/>
+
+{#if $existingAccount}
+<main>
+    {#if myName != "loading..."}
+        <h1>Hello there {myName}</h1>
+        <h1> HEllo</h1>
+    {:else}
+        <!-- <h1>{message}</h1> -->
+        <h2>Hello2 {myAddress}</h2>
+    {/if}
+</main>
+{:else}
+    <form on:submit>
+        <input bind:value={$newName} placeholder="NewName" on:submit={onSubmit}/>
+        <button on:click={onSubmit}>Register</button>
+    </form>
+{/if}
