dd1d89762ecd46ff4d02e48ad7a34fd03de5c1e8
HaseFlrn <florianhase12@gmail.com>
2023-03-27 17:42:32 +0200

    message displaying component

---
+++ src/lib/components/Message.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    export let sender: string;
+    export let timestamp: string;
+    export let text: string;
+    export let myAddress: string;
+
+
+</script>
+
+<div class="wrapper">
+    <div class="sender">{sender}:</div>
+    <div class="text">{text}</div>
+    <div class="time">{timestamp}</div>
+</div>
+
+<style>
+    .wrapper {
+        margin-bottom: 5px;
+        background-color: #242424;
+        border-radius: 8px;
+        padding: 5px
+        
+    }
+    
+    .sender {
+        color: #646cff;
+        text-align: right;
+        text-decoration-line: underline;
+    }
+
+    .text {
+        color: white;
+        text-align: right;
+    }
+
+    .time {
+        color: #707070;
+        text-align: right;
+    }
+
+</style>
