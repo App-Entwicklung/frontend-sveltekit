e3d176fd6567397606710c754fdde784835e26ef
HaseFlrn <florianhase12@gmail.com>
2023-03-27 14:20:02 +0200

    completed init function

--- src/lib/controllers/ChatController.ts
+++ src/lib/controllers/ChatController.ts
@@ -4,24 +4,34 @@ import { writable } from 'svelte/store';
 const baseState = {
 	contactName: 'loading...',
 	contactAddress: 'loading...',
-	messages: []
+	messages: [{}]
 };
 
 class ChatController {
 	#chatStore = writable({ ...baseState });
 	store: any;
-    ethersProvider: EthersProvider | undefined;
+	ethersProvider!: EthersProvider;
 
 	constructor() {
 		this.store = {
-			subscribee: this.#chatStore.subscribe
+			subscribe: this.#chatStore.subscribe
 		};
 	}
 
-	async init() {
+	async init(contact: string) {
 		this.ethersProvider = new EthersProvider();
+		await this.ethersProvider.getReady();
+
+		// expecting string "contactAddress:contactName"
+		const [contactAddress, contactName] = contact.split(':', 1);
+		this.#chatStore.update((s) => ({ ...s, contactAddress, contactName }));
+
+		await this.#loadMessages();
 	}
 
-
+	async #loadMessages() {
+		const messages = await this.ethersProvider.chatContract.getMessages(this.store.contactAddress);
+		this.#chatStore.update((s) => ({ ...s, messages }));
+	}
 }
 export default new ChatController();
