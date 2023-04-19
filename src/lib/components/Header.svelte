d2a90817e5f74f0b0c5183876a53d6fd614bb82c
HaseFlrn <florianhase12@gmail.com>
2023-04-19 16:17:06 +0200

    edited styling; added clut logo+link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,35 +5,50 @@
 <div>
     <a class="home" href="/">Home</a>
     <a class="github" href="https://github.com/App-Entwicklung/frontend-sveltekit"><Icon icon="mdi:github" width="50"/></a>
+    <a class="cult" href="https://cultdao.io/"><img class="cult-logo" src="https://cultdao.io/assets/header-logo2.png" alt="link to cult website"/></a>
 </div>
 
 
 <style>
     div {
+        font-family: Frag!important;
+        font-weight: 1000!important;
+        font-size: large;
+        letter-spacing: 1px!important;
         align-self: center;
         margin: 1px;
         margin-top: -8px;
-        padding: 3vh;
-        background-color: #363636;
+        padding: 2vh;
+        background-color: #1a1a1a;
         border: 1px solid;
         border-radius: 8px;
-        border-color: #646cff;
+        border-color: #901717;
         width: 96.7%;
         word-wrap: break-word;
         position: fixed;
         top: 0;
+        display: flex;
     }
 
     .home {
         border-radius: 8px;
-        background-color: #646cff;
+        background-color: #901717;
         padding: 7px;
-        color: #242424;
+        color: white;
     }
 
     .github {
         position: fixed;
         top: 7px;
         left: 45%;
+    }
+
+    .cult {
+        margin-right: 0;
+        margin-left: auto;
+    }
+    .cult-logo {
+        margin-left: auto;
+        width: 7rem;
     }
 </style>
